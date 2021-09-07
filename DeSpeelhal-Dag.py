b3fc5413303533d7ba0fa30987690476ee0bd6ec
Svengeelhoed <99069588@mydavinci.nl>
2021-09-07 11:58:18 +0200

    Opdrachten leerpad 2 werken met gegevens

---
+++ DeSpeelhal-Dag.py
@@ -0,0 +1,6 @@
+AantalVrienden = 3
+PrijsToegangsticket = 7.45
+Aantal5Minuten = 27
+Prijs5Minuten = 0.37
+
+print(AantalVrienden * PrijsToegangsticket + Aantal5Minuten * Prijs5Minuten)
---
+++ feestlunch.py
@@ -0,0 +1,9 @@
+AantalCroissantjes = 17
+AantalStokbroodjes = 2
+PrijsCroissantjes = 0.39
+PrijsStokbroodjes = 2.78
+AantalKortingsbonnen = 3
+PrijsKortingsbonnen = 0.50
+
+print(AantalCroissantjes * PrijsCroissantjes + AantalStokbroodjes * PrijsStokbroodjes - (AantalKortingsbonnen * PrijsKortingsbonnen))
+
