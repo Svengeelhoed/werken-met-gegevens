77bc0ff3b151cf975fdc3d3af3efca8ac2206a70
Svengeelhoed <99069588@mydavinci.nl>
2021-09-09 10:18:41 +0200

    berekeningen en update voor .py

--- DeSpeelhal-Dag.py
+++ DeSpeelhal-Dag.py
@@ -3,4 +3,8 @@ PrijsToegangsticket = 7.45
 Aantal5Minuten = 27
 Prijs5Minuten = 0.37
 
-print(AantalVrienden * PrijsToegangsticket + Aantal5Minuten * Prijs5Minuten)
+bedrag = (AantalVrienden * PrijsToegangsticket + Aantal5Minuten * Prijs5Minuten)
+
+factuurtekst = " Dit geweldige dagje-uit met " + str(AantalVrienden) + " mensen in de Speelhal met " + str(Aantal5Minuten) + " Keer 5 minuten kost je maar "+ str(bedrag) + " euro!"
+
+print(factuurtekst)
--- feestlunch.py
+++ feestlunch.py
@@ -5,5 +5,10 @@ PrijsStokbroodjes = 2.78
 AantalKortingsbonnen = 3
 PrijsKortingsbonnen = 0.50
 
-print(AantalCroissantjes * PrijsCroissantjes + AantalStokbroodjes * PrijsStokbroodjes - (AantalKortingsbonnen * PrijsKortingsbonnen))
+bedrag = AantalCroissantjes * PrijsCroissantjes + AantalStokbroodjes * PrijsStokbroodjes - AantalKortingsbonnen * PrijsKortingsbonnen
 
+factuurtekst = " De feestlunch kost je bij de bakker " + str(bedrag) + " euro voor de " + str(AantalCroissantjes) + " croissantjes en de " + str(AantalStokbroodjes) + " stokbroden als de " + str(AantalKortingsbonnen) + " kortingsbonnen nog geldig zijn! "
+
+print(factuurtekst)
+
+ 
